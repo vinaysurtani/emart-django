9d41619d3cb36e285887ae6380aa8154edc56902
vinaysurtani <51479061+vinaysurtani@users.noreply.github.com>
2024-07-15 17:26:49 +0530

    django auth

--- emart/settings.py
+++ emart/settings.py
@@ -138,3 +138,15 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: '',
+}
+
+#SMTP Config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'vinaysurtani@gmail.com'
+EMAIL_HOST_PASSWORD = 'zyfm mkef qlqc qvmx'
+EMAIL_USE_TLS = True
